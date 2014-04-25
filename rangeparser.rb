bcce10843095cb0440445f3dd1a20576d6194de9
Greg Weisman <grwgreg@gmail.com>
2014-04-24 20:05:39 -0400

    working method to fill tagbuckets, needs tests

--- rangeparser.rb
+++ rangeparser.rb
@@ -1,7 +1,78 @@
 class RangeParser
+   attr_accessor :tagBuckets
 
   def initialize(range)
     @range = range
+    @tagBuckets = {
+      suited: [],
+      offsuited: [],
+      both: [],
+      single: {}
+    }
+  end
+
+=begin
+  def parseRange(rangeString)
+    expandRangeTags(rangeString)
+    populateRange(@tagBuckets)
+  end
+=end
+
+  def expandRangeTags(rangeString)
+    tags = []
+    rangeTags = rangeString.split(',')
+    rangeTags.each do |rangeTag|
+      rangeTag.strip!
+      tagType = getTagType(rangeTag)
+      raise 'bad tag in rangestring' if tagType.nil?
+      if tagType.to_s.index('spanner').nil?
+        tags = [rangeTag]
+      else
+        tags = expandRangeTag(rangeTag, tagType)
+      end
+      #maybe put in another method, have this return the type and also tags
+      #or take the get tag type out and put in antoher method <--this
+      addToTagBucket(tags, tagType)
+    end
+  end
+
+  def addToTagBucket(tags, tagType)
+    bucket = bucketType(tagType)
+    if bucket == :single
+      tag = tags[0]
+      addSingle(tag)
+    else
+      tags.each do |tag|
+        @tagBuckets[bucket] << tag.slice(0,2)
+      end
+    end
+  end
+
+  def addSingle(tag)
+    hand = tag.slice(0,2)
+    suit = tag.slice(2,4)
+    tagBuckets[:single][hand] ||= []
+    tagBuckets[:single][hand] << suit
+  end
+
+  def bucketType(tagType)
+    types = {
+      suited: [:suitspanner, :suit],
+      offsuited: [:offsuitspanner, :offsuit],
+      both: [:spanner, :pairspanner, :both, :pair],
+      single: [:single]
+    }
+
+    if types[:suited].include?(tagType)
+      bucket = :suited
+    elsif types[:offsuited].include?(tagType)
+      bucket = :offsuited
+    elsif types[:both].include?(tagType)
+      bucket = :both
+    elsif types[:single].include?(tagType)
+      bucket = :single
+    end
+    bucket
   end
 
   def expandRangeTag(rangeTag, tagType)
@@ -34,7 +105,8 @@ class RangeParser
     span.split('')
   end
 
-  def tagType(rangeTag)
+  def getTagType(rangeTag)
+    #order matters here! todo fix it
     tagTypes = {
       spanner: /^[A-Z\d]+-[A-Z\d]+$/,
       suitspanner: /^[A-Z\d]+-[A-Z\d]s+$/,
--- spec/rangeparser.rb
+++ spec/rangeparser.rb
@@ -5,27 +5,33 @@ require '../rangeparser.rb'
 
 rangeParser = RangeParser.new(nil)
 describe rangeParser do
-  it 'has tagType method detrmine rangestring type' do
-    rangeParser.tagType('AKs').should == :suit
-    rangeParser.tagType('AK').should == :both
-    rangeParser.tagType('AK-2').should == :spanner
-    rangeParser.tagType('AK-4s').should == :suitspanner
-    rangeParser.tagType('AK-4o').should == :offsuitspanner
-    rangeParser.tagType('AKo').should == :offsuit
-    rangeParser.tagType('AA').should == :pair
-    rangeParser.tagType('AAcd').should == :single
-    rangeParser.tagType('AA-44').should == :pairspanner
+  it 'has getTagType method detrmine rangestring type' do
+    rangeParser.getTagType('AKs').should == :suit
+    rangeParser.getTagType('AK').should == :both
+    rangeParser.getTagType('AK-2').should == :spanner
+    rangeParser.getTagType('AK-4s').should == :suitspanner
+    rangeParser.getTagType('AK-4o').should == :offsuitspanner
+    rangeParser.getTagType('AKo').should == :offsuit
+    rangeParser.getTagType('AA').should == :pair
+    rangeParser.getTagType('AAcd').should == :single
+    rangeParser.getTagType('AA-44').should == :pairspanner
   end
 
   it 'has expand range method for spanning cards' do
-    puts rangeParser.expandRangeTag('AK-2s', :suitspanner).inspect
-    puts rangeParser.expandRangeTag('QT-4o', :offsuitspanner).inspect
-    puts rangeParser.expandRangeTag('AA-TT', :pairspanner).inspect
-
     rangeParser.expandRangeTag('KJ-4', :spanner)[2].should == 'K9'
     rangeParser.expandRangeTag('KJ-4s', :suitspanner)[2].should == 'K9s'
     rangeParser.expandRangeTag('KJ-4o', :offsuitspanner)[2].should == 'K9o'
     rangeParser.expandRangeTag('KK-2', :pairspanner)[2].should == 'JJ'
   end
+
+  it 'has expand range tags method for taking range string filling tag buckets' do
+    rangeParser.expandRangeTags('AK, QJs, 98cc, 97sc, 98ss, 76-3s, Q9-4, 55, 87-2, 99-22, 76-5s, 98-5o, KJcs')
+    puts 'strt'
+    puts rangeParser.tagBuckets.inspect
+    puts 'here'
+
+    expect { rangeParser.expandRangeTags('AK, afs') }.to raise_error
+  end
+
 end
 
